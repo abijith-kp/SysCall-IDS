1489b1b92ab6bdcf2d8cf3e6bcd6536235877a29
Abijith Kp <abijith.akp@gmail.com>
2014-03-12 17:10:50 +0530

    lcs function-removed printf statements

--- ids_poset.c
+++ ids_poset.c
@@ -21,24 +21,12 @@ int main(int argc, char **argv) {
         char fileN[10];
 	int flag=0, i, j;
 
-        //makePoset(".ids/p1");
-        //printf("\ndsfdsf\n");
-	    // clean();
-        // printPoset(head);
-
-/*        makePoset("input1");
-	clean();
-        printPoset(head);
-*/
+//        printPoset(head);
 
         while(!feof(file)) {
                 fscanf(file, "%s", fileN);
-	//printf("\n %s \n", fileN);
                 makePoset(fileN);
                 clean();
-		// printf("\n%s\n", fileN);
-                // printPoset(head);
-        printf("\n\n");
 		if(flag == 0) {
 			headSIG = head;
 			head = NULL;
@@ -60,13 +48,6 @@ int main(int argc, char **argv) {
 
         printf("\n>> %d \n", lengthPoset(headSIG));
         printf("\n>> %d \n", lengthPoset(head));
-/*        
-        for(i=0; i<200; i++) {
-            for(j=0; j<200; j++) {
-                printf("%d ", val[i][j]);
-            }
-            printf("\n");
-        }
-*/
+        
         return 0;
 }
--- lcs.h
+++ lcs.h
@@ -4,25 +4,6 @@ int length_lcs(struct sysCPS *headRef1, struct sysCPS *headRef2) {
     l1 = lengthPoset(headRef1);
     l2 = lengthPoset(headRef2);
 
-    /*
-    if(val[l1][l2] >= 0) {
-        return val[l1][l2];
-    }
-    else {
-    }
-    
-    if((headRef1->call1 == headRef2->call1) && (headRef1->call2 == headRef2->call2)) {
-        printf("%s\t%s\n", headRef1->call1, headRef1->call2);
-        t = 1 + length_lcs(headRef1->next, headRef2->next);
-    }
-    else if((headRef1->call1 != headRef2->call1) || (headRef1->call2 != headRef2->call2)) {
-        printf("\n>>>>>><<<<<<   %d %d\n", lengthPoset(headRef1), lengthPoset(headRef2));
-        t2 = length_lcs(headRef1, headRef2->next);
-        t1 = length_lcs(headRef1->next, headRef2);
-        t = (t1>t2)?t1:t2;
-    }
-    */
-
     if((headRef2 == NULL) || (headRef1 == NULL)) {
         return 0;
     }
@@ -31,15 +12,11 @@ int length_lcs(struct sysCPS *headRef1, struct sysCPS *headRef2) {
         return val[l1][l2];
     }
 
-    // printf("%s\t%s\n", headRef1->call1, headRef2->call1);
     if((!strcmp(headRef1->call1, headRef2->call1)) && (!strcmp(headRef1->call2, headRef2->call2))) {
-        printf("\nsdf\n");
-        // printf("%s\t%s\n", headRef1->call1, headRef1->call2);
         t1 = length_lcs(headRef1->next, headRef2->next);
         val[l1][l2] = (val[l1][l2] > (1 + t1))?val[l1][l2]:(1+t1);
     }
     else if((strcmp(headRef1->call1, headRef2->call1)) || (strcmp(headRef1->call2, headRef2->call2))) {
-        // printf("\n>>>>>><<<<<<   %d %d\n", lengthPoset(headRef1), lengthPoset(headRef2));
         t2 = length_lcs(headRef1, headRef2->next);
         t1 = length_lcs(headRef1->next, headRef2);
         t =  (t1>t2)?t1:t2;
