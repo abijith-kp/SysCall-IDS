d8abedaac9eba8faf46b51ce8f8e841b66f94930
Abijith Kp <abijith.akp@gmail.com>
2014-04-01 13:55:51 +0000

    started function string_lcs

--- lcs.h
+++ lcs.h
@@ -26,6 +26,45 @@ int length_lcs(struct sysCPS *headRef1, struct sysCPS *headRef2) {
     return val[l1][l2];
 }
 
+
+int string_lcs(struct sysCPS *headRef1, struct sysCPS *headRef2) {
+    int l1=0, l2=0, t=0, t1=0, t2=0, t3=0;
+    struct sysCPS *tmpHead;
+
+    l1 = lengthPoset(headRef1);
+    l2 = lengthPoset(headRef2);
+
+    if((headRef2 == NULL) || (headRef1 == NULL)) {
+        return 0;
+    }
+
+    if(val[l1][l2] >= 0) {
+        return val[l1][l2];
+    }
+
+    if((!strcmp(headRef1->call1, headRef2->call1)) && (!strcmp(headRef1->call2, headRef2->call2))) {
+	tmpHead = (struct sysCPS *)malloc(sizeof(struct sysCPS *));
+	tmpHead->call1 = strdup(headRef1->call1);
+	tmpHead->call2 = strdup(headRef1->call2);
+	// if(retHead == NULL) {
+	tmpHead->next = retHead;
+	retHead = tmpHead;
+	// }
+        t1 = length_lcs(headRef1->next, headRef2->next);
+        val[l1][l2] = (val[l1][l2] > (1 + t1))?val[l1][l2]:(1+t1);
+    }
+    else if((strcmp(headRef1->call1, headRef2->call1)) || (strcmp(headRef1->call2, headRef2->call2))) {
+        t2 = length_lcs(headRef1, headRef2->next);
+        t1 = length_lcs(headRef1->next, headRef2);
+        t =  (t1>t2)?t1:t2;
+        val[l1][l2] = (val[l1][l2]>t)?val[l1][l2]:t;
+    }
+    
+    return val[l1][l2];
+}
+
+
+struct sysCPS *retHead;
 char *string_lcs(struct sysCPS *headRef1, struct sysCPS *headRef2) {
 	int l1=0, l2=0, t=0, t1=0, t2=0, t3=0;
 
